29fd57e8a0472be405b84ff9f55e5b7cc142e3c2
RosibertoSantos <rosiberto6@hotmail.com>
2022-01-11 02:37:21 -0300

    MovieCard

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -9,7 +9,7 @@ function MovieScore(){
     return(
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStarsars />
+            <MovieStars />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
